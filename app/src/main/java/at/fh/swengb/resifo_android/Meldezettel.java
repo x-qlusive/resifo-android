0cd965f038d79739377be09d8e48eb16df598902
X-qlusive <extasi@gmx.net>
2016-12-23 22:13:06 +0100

    Projekt und geforderte Fiels e.g. proposal.md erstellt.

---
+++ app/src/main/java/at/fh/swengb/resifo_android/Meldezettel.java
@@ -0,0 +1,13 @@
+package at.fh.swengb.resifo_android;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Meldezettel extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_meldezettel);
+    }
+}
