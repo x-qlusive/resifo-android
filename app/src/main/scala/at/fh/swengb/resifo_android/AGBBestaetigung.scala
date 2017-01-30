da9a32690136d6871cefa7a3c44cca02290dd2d1
jashanic14 <jashanica@live.at>
2017-01-30 20:30:22 +0100

    AGBBestatigung (Class) + .xml View added

---
+++ app/src/main/scala/at/fh/swengb/resifo_android/AGBBestaetigung.scala
@@ -0,0 +1,11 @@
+package at.fh.swengb.resifo_android
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+class AGBBestaetigung extends AppCompatActivity {
+  override protected def onCreate(savedInstanceState: Bundle) {
+    super.onCreate(savedInstanceState)
+    setContentView(R.layout.activity_agbbestaetigung)
+  }
+}
